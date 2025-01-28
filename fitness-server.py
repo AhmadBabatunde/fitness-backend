7d6476c07836cb57d36a375b1178a9b48e1a817e
Ahmad Babatunde <ahmadtijani1639@gmail.com>
2025-01-28 17:33:15 +0100

    cors

--- fitness-server.py
+++ fitness-server.py
@@ -17,7 +17,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["https://fitness-frontend-ten-dusky.vercel.app"])
+CORS(app)
 
 # Contextualize question
 contextualize_q_system_prompt = (
